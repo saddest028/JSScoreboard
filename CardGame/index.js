eb4e020aaff4b7e9fdb031066664046d96450beb
Miguel Salingohay <miguelsalingohay1@gmail.com>
2022-08-18 20:21:26 +0800

    card game

---
+++ CardGame/index.js
@@ -0,0 +1,13 @@
+let firstCard = 3;
+let secondCard = 8;
+let sum = firstCard + secondCard;
+
+if(sum < 21){
+    console.log("less than 21");
+}
+else if(sum === 21){
+    console.log("equals 21");
+}
+else{
+    console.log("over 21");
+}
